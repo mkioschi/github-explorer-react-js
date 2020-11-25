0c1bbc3acb58d87fcdd308c7d846b4547949a92d
Mayko Kioschi <mkioschi@gmail.com>
2020-11-24 23:28:10 -0300

    Styled dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import logoImg from "../../assets/logo.svg";
+import { Title, Form, Repositories } from "./styles";
+import { FiChevronRight } from "react-icons/fi";
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/2711935?s=460&u=4c65cb439efc87350315b5984902f720a02a51cf&v=4"
+            alt="Mayko Kioschi"
+          />
+          <div>
+            <strong>nome/repositório</strong>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/2711935?s=460&u=4c65cb439efc87350315b5984902f720a02a51cf&v=4"
+            alt="Mayko Kioschi"
+          />
+          <div>
+            <strong>nome/repositório</strong>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/2711935?s=460&u=4c65cb439efc87350315b5984902f720a02a51cf&v=4"
+            alt="Mayko Kioschi"
+          />
+          <div>
+            <strong>nome/repositório</strong>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
